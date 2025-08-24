8b65ae5e89e3b2433f2a321c35f4ee5792dd96fd
TejalK-4954 <tejalkauthe11@gmail.com>
2025-08-24 21:50:06 +0530

    volatile

--- Practice_day3/src/multi/volatiledemo1.java
+++ Practice_day3/src/multi/volatiledemo1.java
@@ -1,58 +1,57 @@
 package multi;
 
 public class volatiledemo1 {
-
-
-		  volatile static boolean flag=false;
-		
+	
+	volatile static boolean flag=false;
+	
+ public static void main(String[] args) throws InterruptedException {
+	
+	 Thread th=new Thread()
+			 {
 		 
-		public static void main(String[] args) throws InterruptedException {
-			
-			Thread thread=new Thread() {
-				
-				public void run()
-				{
-					try {
-						Thread.sleep(10);
-						flag=true;
-						System.out.println("set flag to the true..");
-					} catch (InterruptedException e) {
-						// TODO Auto-generated catch block
-						e.printStackTrace();
-					}
-				}
-			};
-			
-	         
-			Thread thread2=new Thread()
-					{
-				  public void run()
-				  
-				  {
-					 while(true) {
-					  if(!flag)
-					  {
-						  while(!flag) {
-							  
-						  }
-						  System.out.println("waiting for the flag to become the true");
-						  
-					  }
-					  else
-					  {
-						  System.out.println("flag set to true..executing...");
-						  break;
-					  }
-					 }
-				  
-				}};
-					
-					thread.start();
-					thread2.start();
-					
-					thread.join();
-					thread2.join();
-		}
+		 public void run()
+		 {
+			 try {
+				Thread.sleep(10);
+				flag=true;
+				System.out.println("set flag to true..");
+			} catch (InterruptedException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+		 }
+	};
+	
+	Thread th2=new Thread()
+	{
+  public void run()
+  
+  {
+	 while(true) {
+	  if(!flag)
+	  {
+		  while(!flag) {
+			  
+		  }
+		  System.out.println("waiting for the flag to become the true");
+		  
+	  }
+	  else
+	  {
+		  System.out.println("flag set to true..executing...");
+		  break;
+	  }
+	 }
+  
+}};
+
+th.start();
+th2.start();
+
+th.join();
+th2.join();
+}
+
 
 	}
 
