4572d1340ff56ebe600a60950b1cf4cefe1908b3
TejalK-4954 <tejalkauthe11@gmail.com>
2025-08-26 20:04:24 +0530

    instance reference

--- practice_day4/src/methodreference/InstanceReference.java
+++ practice_day4/src/methodreference/InstanceReference.java
@@ -1,44 +1,70 @@
 package methodreference;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class InstanceReference {
 
 	public static void main(String[] args)
 	  {
-	        	
+	      Student student =new Student(1,"ABC","abc@gmail.com",33);
+	      
+	      List<Student> list=student.createlist();
 		     
+	      filter_student filter=new filter_student(student);
+	      
+	      list.stream().filter(filter::filter_stud).forEach((i)->System.out.println(i.email));
 		
 	  }
 
 }
 
-
-class car
+class Student 
 {
    int id;
    String name;
-   double price;
+   String email;
+   int mark;
    
-   public car(int id,String name,double price)
+public Student(int id, String name, String email, int marks) {
+	this.id = id;
+	this.name = name;
+	this.email = email;
+	this.mark = marks;
+}
+   
+public List<Student> createlist()
+{
+  List<Student> list=new ArrayList<Student>();
+  
+  list.add(this);
+  list.add(new Student(this.id+1,this.name+"a",this.email+"a",this.mark+1));
+  list.add(new Student(this.id+2,this.name+"b",this.email+"b",this.mark+2));
+  list.add(new Student(this.id+3,this.name+"c",this.email+"c",this.mark+3));
+
+     return list;
+ }
+   
+}
+
+class filter_student
+{
+   Student student;
+   
+   public filter_student(Student student)
    {
-	   this.id=id;
-	   this.name=name;
-	   this.price=price;
+	   this.student=student;
    }
    
-   public boolean checkprice()
+   public boolean filter_stud(Student student)
    {
-	   if(this.price>100.0)
+	   if(student.mark>35)
 	   {
 		   return true;
 	   }
 	   return false;
    }
-
-     @Override
-     public String toString() 
-     {
-	   return "car [id=" + id + ", name=" + name + ", price=" + price + "]";
-     }
-     
-}
-
+	
+	
+}  
+ 
