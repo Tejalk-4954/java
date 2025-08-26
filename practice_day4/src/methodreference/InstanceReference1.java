f9102c8cb85089d76edf052417a818293679ad03
TejalK-4954 <tejalkauthe11@gmail.com>
2025-08-26 21:03:37 +0530

    constructorReferce

---
+++ practice_day4/src/methodreference/InstanceReference1.java
@@ -0,0 +1,70 @@
+package methodreference;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class InstanceReference1 {
+
+	public static void main(String[] args)
+	  {
+	      Student student =new Student(1,"ABC","abc@gmail.com",33);
+	      
+	      List<Student> list=student.createlist();
+		     
+	      filter_student filter=new filter_student(student);
+	      
+	      list.stream().filter(filter::filter_stud).forEach((i)->System.out.println(i.email));
+		
+	  }
+
+}
+
+class Student 
+{
+   int id;
+   String name;
+   String email;
+   int mark;
+   
+public Student(int id, String name, String email, int marks) {
+	this.id = id;
+	this.name = name;
+	this.email = email;
+	this.mark = marks;
+}
+   
+public List<Student> createlist()
+{
+  List<Student> list=new ArrayList<Student>();
+  
+  list.add(this);
+  list.add(new Student(this.id+1,this.name+"a",this.email+"a",this.mark+1));
+  list.add(new Student(this.id+2,this.name+"b",this.email+"b",this.mark+2));
+  list.add(new Student(this.id+3,this.name+"c",this.email+"c",this.mark+3));
+
+     return list;
+ }
+   
+}
+
+class filter_student
+{
+   Student student;
+   
+   public filter_student(Student student)
+   {
+	   this.student=student;
+   }
+   
+   public boolean filter_stud(Student student)
+   {
+	   if(student.mark>35)
+	   {
+		   return true;
+	   }
+	   return false;
+   }
+	
+	
+}  
+ 
---
+++ practice_day4/src/methodreference/constructorRef.java
@@ -0,0 +1,57 @@
+package methodreference;
+
+import java.util.function.BiFunction;
+import java.util.function.Function;
+
+public class constructorRef {
+
+	public static void main(String[] args)
+	{
+		 BiFunction<String,String,Object> fun=car::new;
+		 
+		 fun.apply("Mustang", "Ford");
+		 
+		 Function<String ,car> fun3=car::new;
+		 fun3.apply("Alto");
+		 
+		 Ternary<Integer,String, String, String,car > ter=car::new;
+		 ter.tertiary(12, "alto", "1234556", "tatapunch");
+		
+	}
+
+}
+
+
+class car
+{
+   String name;
+   String company;
+   
+   public car(String name,String company)
+   {
+	   this.name=name;
+	   this.company=company;
+	   System.out.println("object created:"+name);
+   }
+   
+   public car(String name)
+   {
+	   this.name=name;
+	   
+	   System.out.println("object createss:"+name);
+   }
+   
+  
+   public car(int id, String city, String pin, String state)
+	{
+		System.out.println("Object create:"+id+city+pin+state);
+	}
+}
+
+
+
+interface Ternary<A,B,C,D,R>
+{
+	
+	public R tertiary(A a,B b,C c,D d);
+}
