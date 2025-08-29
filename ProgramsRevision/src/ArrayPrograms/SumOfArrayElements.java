8b24e5f4b79c8738075ca700f7afa91368533054
TejalK-4954 <tejalkauthe11@gmail.com>
2025-08-29 09:59:23 +0530

    array programs

---
+++ ProgramsRevision/src/ArrayPrograms/SumOfArrayElements.java
@@ -0,0 +1,29 @@
+package ArrayPrograms;
+
+import java.util.Scanner;
+
+public class SumOfArrayElements {
+
+	public static void main(String[] args) {
+     
+		Scanner sc=new Scanner(System.in);
+		System.out.println("enter size::");
+		int size = sc.nextInt();
+		
+		int arr[]=new int[size];
+        
+		System.out.println("enter array elements::");
+		for(int i=0;i<size;i++)
+		{
+			arr[i]=sc.nextInt();
+		}
+		
+		int sum=0;
+		for(int i=0;i<size;i++)
+		{
+			sum+=arr[i];
+		}
+         
+		System.out.println("Sum of Array Elements::"+sum);
+	}
+}
