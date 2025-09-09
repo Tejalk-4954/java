002d7d0eb56600dd6af17ad50f80cbf314f227a4
TejalK-4954 <tejalkauthe11@gmail.com>
2025-09-09 23:26:01 +0530

    jdbc api

--- practice_day5/src/JDBCApi/jdbc1.java
+++ practice_day5/src/JDBCApi/jdbc1.java
@@ -10,26 +10,30 @@ public class jdbc1 {
 
 	public static void main(String[] args) throws ClassNotFoundException, SQLException {
 		
-		String name;
-		int id;
-		
-		Scanner sc=new Scanner(System.in);
-		System.out.println("enter name::");
-		name=sc.nextLine();
-		
-		System.out.println("please enter the id::");
-		id=sc.nextInt();
+//		String name;
+//		int id;
+//		
+//		Scanner sc=new Scanner(System.in);
+//		System.out.println("enter name::");
+//		name=sc.nextLine();
+//		
+//		System.out.println("please enter the id::");
+//		id=sc.nextInt();
 		
 		Class.forName("com.mysql.cj.jdbc.Driver");
 		
-		Connection con=DriverManager.getConnection("jdbc::mysql://localhost:3306/Demo","root","tejal");
-	    
-	    System.out.println("***Connection open***");
-	    
-	    try(con)
-	    {
-	    	Statement stmt=con.createStatement();
-	    	
-	    }
+		Connection con=DriverManager.getConnection("jdbc:mysql://localhost:3306/Demo","root","tejal");
+		
+		
+		Statement stmt=con.createStatement();
+		String sql="create table student(id int, name varchar(50))";
+	   
+		boolean result=stmt.execute(sql);
+		
+		System.out.println(result);
+		
+		con.close();
+
+		
 	}
 }
