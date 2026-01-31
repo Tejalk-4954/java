f050eb91e3ed49ab449131f36e84475aacb2017b
TejalK-4954 <tejalkauthe11@gmail.com>
2026-01-31 12:16:02 +0530

    loops programs

--- ProgramsRevision/src/LoopsProgram/first.java
+++ ProgramsRevision/src/LoopsProgram/first.java
@@ -16,32 +16,51 @@ public class first {
 //	 }
 //	 
 	 //nested if
-	 
-	 String college="MIT";
+//	 
+//	 String college="MIT";
 	 int marks=80;
-	 String branch="IT";
+//	 String branch="IT";
+//	 
+//	 if(college=="MIT")
+//	 { 
+//		 if(branch=="IT")
+//		 {
+//			 if(marks>=80)
+//			 {
+//				 System.out.print("Eligible");
+//			 }
+//			 else
+//			 {
+//				 System.out.println("marks are less than 80");
+//			 }
+//		 }
+//		 else
+//		 {
+//			System.out.println("branch is diff"); 
+//		 }
+//	 }
+//	 else
+//	 {
+//		 System.out.println("student is not from MIT");
+//	 }
+//	 
+	 //if-else-if 
 	 
-	 if(college=="MIT")
-	 { 
-		 if(branch=="IT")
-		 {
-			 if(marks>=80)
-			 {
-				 System.out.print("Eligible");
-			 }
-			 else
-			 {
-				 System.out.println("marks are less than 80");
-			 }
-		 }
-		 else
-		 {
-			System.out.println("branch is diff"); 
-		 }
+	 if(marks>=75 &&marks<=100)
+	 {
+		 System.out.println("O-Grade");
+	 }
+	 else if(marks>=50 && marks<75)
+	 {
+		 System.out.println("A-Grade");
+	 }
+	 else if(marks>=35 && marks<50)
+	 {
+		System.out.println("B-Grade"); 
 	 }
 	 else
 	 {
-		 System.out.println("student is not from MIT");
+		 System.out.println("fail");
 	 }
   }
 }
