763ec631f3c8521506b1fc4a81f6a09038a8dd16
TejalK-4954 <tejalkauthe11@gmail.com>
2026-01-31 11:03:58 +0530

    program revision

--- ProgramsRevision/src/TwoDArray/AddTwoMatrices.java
+++ ProgramsRevision/src/TwoDArray/AddTwoMatrices.java
@@ -1,5 +1,7 @@
 package TwoDArray;
 
+import java.util.Arrays;
+
 public class AddTwoMatrices {
 public static void main(String[] args) {
 	
@@ -37,6 +39,6 @@ public static void main(String[] args) {
 		System.out.println();
 	}
 	
-	
+//	System.out.println(Arrays.deepToString(sumMatrix));
   }
 }
